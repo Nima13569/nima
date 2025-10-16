590326a52a59d259edba1be4cc871e64be20edb3
Nima13569 <89773262+Nima13569@users.noreply.github.com>
2025-10-16 15:52:20 +0330

    Add files via upload

---
+++ proje3_084908.py
@@ -0,0 +1,12 @@
+import math
+def sumr():
+    y=int(input())
+    w=int(input())
+    x=int(input())
+    z=y-w*x
+    n=y-w*x
+    total = sum[(z,n)]
+    print(total)
+    print(z)
+    print(n)
+sumr()
